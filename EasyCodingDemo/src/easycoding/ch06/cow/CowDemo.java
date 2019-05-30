48b45df23d31a7f6213dc5a7f2a4ab8e9677add7
Rain Liu <liuwenbin168@126.com>
2019-05-30 15:53:25 +0800

    Added fail-fast

---
+++ EasyCodingDemo/src/easycoding/ch06/cow/CowDemo.java
@@ -0,0 +1,25 @@
+package easycoding.ch06.cow;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CopyOnWriteArraySet;
+
+public class CowDemo {
+    public static void main(String[] args) {
+
+        Set set;
+
+        CopyOn
+
+        List<Long> copy = new CopyOnWriteArrayList<>();
+
+        long start = System.nanoTime();
+        for (int i = 0; i < 20*10000; i++) {
+            copy.add(System.nanoTime());
+        }
+        long end = System.nanoTime();
+        System.out.println((end - start) / 1000 / 1000);
+    }
+}
---
+++ EasyCodingDemo/src/easycoding/ch06/iterable/IterableDemo.java
@@ -0,0 +1,9 @@
+package easycoding.ch06.iterable;
+
+import java.util.List;
+
+public class IterableDemo {
+    public static void main(String[] args) {
+        List list;
+    }
+}
